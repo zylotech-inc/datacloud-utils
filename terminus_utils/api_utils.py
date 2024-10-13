fe5397d539eff40fe2527e5fa20ff1ea52d03c1e
vivek11261 <vivek.verma@terminus.com>
2024-10-13 23:23:11 +0530

    add one conditon to handle ranges value

--- terminus_utils/api_utils.py
+++ terminus_utils/api_utils.py
@@ -73,21 +73,18 @@ def transform_employee_revenue_value(input_str: str):
             inferred_value = False
             input_str = input_str.lower().replace(',', '').strip()
 
-            def convert_value(value_str, suffix):
+            def convert_value(value_str):
                 """Helper function to convert individual string values to absolute numbers."""
-                if 'k' in suffix or 'thousand' in value_str:
+                if 'k' in value_str or 'thousand' in value_str:
                     return float(value_str.replace('k', '').replace('thousand', '')
                                  .replace('$', '').strip()) * 1_000
-                elif 'cr' in suffix or 'crore' in value_str:
-                    value_in_cr = float(value_str.replace('crore', '').replace('cr', '')) * 10_000_000
-                    return convert_inr_to_usd(value_in_cr)
-                elif 'm' in suffix or 'million' in value_str:
+                elif 'm' in value_str or 'million' in value_str:
                     return float(value_str.replace('million', '').replace('m', '')
                                  .replace('$', '').strip()) * 1_000_000
-                elif 'b' in suffix or 'billion' in value_str:
+                elif 'b' in value_str or 'billion' in value_str:
                     return float(value_str.replace('billion', '').replace('b', '')
                                  .replace('$', '').strip()) * 1_000_000_000
-                elif 't' in suffix or 'trillion' in value_str:
+                elif 't' in value_str or 'trillion' in value_str:
                     return float(value_str.replace('trillion', '').replace('t', '')
                                  .replace('$', '').strip()) * 1_000_000_000_000
                 else:
@@ -104,37 +101,35 @@ def transform_employee_revenue_value(input_str: str):
                     suffix_b = match.group(6)
                     return a, b, suffix_a, suffix_b
                 return None, None, None, None
-
+            
             if '-' in input_str:
                 lower, upper, suffix_a, suffix_b = split_value_parts(input_str)
                 if lower is None or upper is None:
                     return None, False
-                if suffix_a and suffix_b:
-                    lower_value = convert_value(lower + suffix_a, suffix_a)
-                    upper_value = convert_value(upper + suffix_b, suffix_b)
+                if not suffix_a and suffix_b:
+                    suffix_a = suffix_b
+                if not suffix_b and suffix_a:
+                    suffix_b = suffix_a
+                if suffix_b == 'k' and 100 <= float(lower) <= 900:
+                    lower_value = convert_value(lower)
                 else:
-                    common_suffix = suffix_a if suffix_a else suffix_b
-                    lower_value = convert_value(lower + (suffix_a if suffix_a else ''), common_suffix)
-                    upper_value = convert_value(upper + (suffix_b if suffix_b else ''), common_suffix)
+                    lower_value = convert_value(lower + suffix_a)
+                upper_value = convert_value(upper + suffix_b)
                 output_value = (lower_value + upper_value) / 2
                 inferred_value = True
             elif '<' in input_str:
-                value = convert_value(input_str.replace('<', '').replace('>', '').strip(), input_str)
-
-                output_value = value*0.5
+                value = convert_value(input_str.replace('<', '').strip())
+                output_value = value * 0.5
                 inferred_value = True
             elif '>' in input_str:
-                value = convert_value(input_str.replace('<', '').replace('>', '').strip(), input_str)
+                value = convert_value(input_str.replace('>', '').strip())
                 inferred_value = True
-                output_value = value + value*0.5  # => (value + (value*2))/2
-                return output_value, inferred_value
-
+                output_value = value * 1.5 #need to discuss
             else:
-                output_value = convert_value(input_str, input_str)
+                output_value = convert_value(input_str)
             return int(round(output_value)), inferred_value
         return None, None
     except (ValueError, TypeError):
-        # logger.exception(f"{input_str} is not a valid value.")
         return None, False
 
 
